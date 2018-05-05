c7f50e23495a05cbdff4f7e1c7b27a3299a433f0
Robin Sliwa <rorrosliwa@gmail.com>
2018-05-05 22:03:33 +0200

    Planck and Phaser working together

--- js/game/Game.js
+++ js/game/Game.js
@@ -4,37 +4,34 @@ var Global = Global || {};
 Global.Game = function()
 {
 	this.step = 0;
+	this.planckWorld = planck.World({ gravity: planck.Vec2(0, 10) });
 };
+
+var CIRCLE_RADIUS = 10;
 
 Global.Game.prototype.create = function ()
 {
 	Global.game.stage.backgroundColor = '#eeeeee';
+	// NOTE: The physics engine might fuck up if we scale it
+	// So scale the renderer instead
+	this.game.world.scale.set(GRAPHICS_SCALE, GRAPHICS_SCALE);
 
 	this.graphics = Global.game.add.graphics(0, 0);
-	this.graphics.beginFill(0xFF0000, 1);
-	this.graphics.drawCircle(50, 50, 100);
-
-	/* Box2dJs */
-
-	setupWorld();
-	ctx = $('canvas').getContext('2d');
-	var canvasElm = $('canvas');
-	canvasWidth = parseInt(canvasElm.width);
-	canvasHeight = parseInt(canvasElm.height);
-	canvasTop = parseInt(canvasElm.style.top);
-	canvasLeft = parseInt(canvasElm.style.left);
-	Event.observe('canvas', 'click', function(e) {
-		//setupNextWorld();
-		if (Math.random() < 0.5) 
-			demos.top.createBall(world, Event.pointerX(e) - canvasLeft, Event.pointerY(e) - canvasTop);
-		else 
-			createBox(world, Event.pointerX(e) - canvasLeft, Event.pointerY(e) - canvasTop, 10, 10, false);
-	});
-	Event.observe('canvas', 'contextmenu', function(e) {
-		if (e.preventDefault) e.preventDefault();
-		setupPrevWorld();
-		return false;
-	});
+	this.circleBody = this.planckWorld.createDynamicBody(planck.Vec2(5, 5));
+	this.circleBody.createFixture(planck.Circle(CIRCLE_RADIUS), { density: 1.0, friction: 0.9 });
+	
+	this.coyoteSprite = this.add.sprite(0, 0, "coyote");
+	this.coyoteSprite.scale.set(0.1, 0.1);
+	
+	var ground = this.planckWorld.createBody();
+	var groundFixtureDefinition = {
+		density: 0.0,
+		friction: 0.6
+	}
+	ground.createFixture(planck.Edge(
+		planck.Vec2(0, GAME_HEIGHT),
+		planck.Vec2(GAME_WIDTH, GAME_HEIGHT)
+	), groundFixtureDefinition);
 };
 
 Global.Game.prototype.preRender = function ()
@@ -43,121 +40,27 @@ Global.Game.prototype.preRender = function ()
 
 Global.Game.prototype.update = function ()
 {
-	this.handleBox2D();
+	this.planckWorld.step(1/60);
 };
 
-Global.Game.prototype.handleBox2D = function ()
-{
-	var stepping = false;
-	var timeStep = 1.0/60;
-	var iteration = 1;
-	world.Step(timeStep, iteration);
-	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	this.drawWorld(world, ctx);
-};
 
 Global.Game.prototype.render = function ()
 {
-};
-
-
-
-Global.Game.prototype.drawWorld = function (world, context) {
-	this.graphics.clear(0xFFFFFF);
-
-	for (var j = world.m_jointList; j; j = j.m_next) {
-		this.drawJoint(j, context);
-	}
-	for (var b = world.m_bodyList; b; b = b.m_next) {
-		for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
-			this.drawShape(s, context);
+	/*for (var body = this.planckWorld.getBodyList(); body; body = body.getNext()) {
+		for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+		// draw or update fixture
 		}
-	}
-};
-
-Global.Game.prototype.drawJoint = function (joint, context) {
-	var b1 = joint.m_body1;
-	var b2 = joint.m_body2;
-	var x1 = b1.m_position;
-	var x2 = b2.m_position;
-	var p1 = joint.GetAnchor1();
-	var p2 = joint.GetAnchor2();
-	context.strokeStyle = '#00eeee';
-	context.beginPath();
-	switch (joint.m_type) {
-	case b2Joint.e_distanceJoint:
-		context.moveTo(p1.x, p1.y);
-		context.lineTo(p2.x, p2.y);
-		break;
-
-	case b2Joint.e_pulleyJoint:
-		// TODO
-		break;
-
-	default:
-		if (b1 == world.m_groundBody) {
-			context.moveTo(p1.x, p1.y);
-			context.lineTo(x2.x, x2.y);
-		}
-		else if (b2 == world.m_groundBody) {
-			context.moveTo(p1.x, p1.y);
-			context.lineTo(x1.x, x1.y);
-		}
-		else {
-			context.moveTo(x1.x, x1.y);
-			context.lineTo(p1.x, p1.y);
-			context.lineTo(x2.x, x2.y);
-			context.lineTo(p2.x, p2.y);
-		}
-		break;
-	}
-	context.stroke();
-};
-
-Global.Game.prototype.drawShape = function (shape, context) {
-	context.strokeStyle = '#ffffff';
-	context.beginPath();
-	switch (shape.m_type) {
-	case b2Shape.e_circleShape:
-		{
-			var circle = shape;
-			var pos = circle.m_position;
-			var r = circle.m_radius;
-			var segments = 16.0;
-			var theta = 0.0;
-			var dtheta = 2.0 * Math.PI / segments;
-			// draw circle
-			context.moveTo(pos.x + r, pos.y);
-			for (var i = 0; i < segments; i++) {
-				var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
-				var v = b2Math.AddVV(pos, d);
-				context.lineTo(v.x, v.y);
-				theta += dtheta;
-			}
-			context.lineTo(pos.x + r, pos.y);
-
-			this.graphics.beginFill(0xFF0000, 1);
-			this.graphics.drawCircle(pos.x, pos.y, r*2);
-
-			// draw radius
-			context.moveTo(pos.x, pos.y);
-			var ax = circle.m_R.col1;
-			var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
-			context.lineTo(pos2.x, pos2.y);
-		}
-		break;
-	case b2Shape.e_polyShape:
-		{
-			var poly = shape;
-			var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
-			context.moveTo(tV.x, tV.y);
-			for (var i = 0; i < poly.m_vertexCount; i++) {
-				var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
-				context.lineTo(v.x, v.y);
-			}
-			context.lineTo(tV.x, tV.y);
-		}
-		break;
-	}
-	context.stroke();
+	}*/
+	this.graphics.clear();
+	this.graphics.beginFill(0xFF0000, 1);
+	var circlePos = this.circleBody.getPosition();
+	this.graphics.drawCircle(circlePos.x, circlePos.y, CIRCLE_RADIUS * 2);
+	
+	// Draw ground
+	this.graphics.lineStyle(1, 0x000000, 1.0);
+	this.graphics.moveTo(0, GAME_HEIGHT);
+	this.graphics.lineTo(GAME_WIDTH, GAME_HEIGHT);
+	
+	this.coyoteSprite.centerX = circlePos.x;
+	this.coyoteSprite.centerY = circlePos.y;
 };
